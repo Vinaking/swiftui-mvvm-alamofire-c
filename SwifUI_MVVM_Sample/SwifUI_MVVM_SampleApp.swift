6804d95b440cf092ab039b1f2638c86e199ec7b8
TungH <tungh@ichiisoft.com>
2024-04-22 10:36:56 +0700

    Initial Commit

---
+++ SwifUI_MVVM_Sample/SwifUI_MVVM_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUI_MVVM_SampleApp.swift
+//  SwifUI_MVVM_Sample
+//
+//  Created by Tùng on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwifUI_MVVM_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
